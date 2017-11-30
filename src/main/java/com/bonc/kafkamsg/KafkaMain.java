7b18dab63fac5a883ffa4eed6d2312044387f2c2
Scar <18204317968@users.github.com>
2017-11-30 10:18:07 +0800

    Initial commit

---
+++ src/main/java/com/bonc/kafkamsg/KafkaMain.java
@@ -0,0 +1,13 @@
+package com.bonc.kafkamsg;
+
+/**
+ * Hello world!
+ *
+ */
+public class KafkaMain 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
